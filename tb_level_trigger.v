36a85f03d720440f7f2714fd9bd9e5570c3ba1bd
K SREE SAI VENKAT <2003ssv@gmail.com>
2025-06-25 15:30:21 +0530

    Update tb_level_trigger.v

--- tb_level_trigger.v
+++ tb_level_trigger.v
@@ -1,31 +1,23 @@
 `timescale 1ns/1ps
 
-module tb_level_triggered_data_shift;
+module tb_level_trigger;
 
     reg clk;
     wire sclk;
     wire sda;
 
-    // Instantiate the DUT (Design Under Test)
-    level_timed_data_shift dut (
-        .clk(clk),
-        .sclk(sclk),
-        .sda(sda)
-    );
+    level_trigger dut ( .clk(clk), .sclk(sclk), .sda(sda) );
 
-    // Generate system clock (10ns period = 100MHz)
-    initial begin
-        clk = 0;
-        forever #5 clk = ~clk;
-    end
+    initial 
+        begin
+            clk = 0;
+            forever #5 clk = ~clk;
+        end
 
-    // Run simulation
-    initial begin
-        $dumpfile("waveform.vcd");  // For GTKWave
-        $dumpvars(0, tb_level_timed_data_shift);
-
-        #2000;  // Run simulation long enough to view several bits
-        $finish;
-    end
+    initial 
+        begin
+          #2000;  
+          $finish;
+        end
 
 endmodule
